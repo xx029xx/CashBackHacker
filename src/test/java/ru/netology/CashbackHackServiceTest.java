a531f018f709781c66539f1232074cf4cad191fd
Natalia Zvereva <xx029xx@ya.ru>
2023-04-08 13:19:31 +0300

    chore: testunit4

--- src/test/java/ru/netology/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/CashbackHackServiceTest.java
@@ -1,45 +1,45 @@
 package ru.netology;
 
-import org.testng.annotations.Test;
-
-import static org.testng.Assert.*;
+//import org.testng.annotations.Test;
+import static org.junit.Assert.*;
+//import static org.testng.Assert.*;
 public class CashbackHackServiceTest {
     CashbackHackService service = new CashbackHackService();
 
-    @org.testng.annotations.Test
+    @org.junit.Test
     public void testBoundaryValues() {
-        org.testng.Assert.assertThrows(RuntimeException.class, () -> {
+        org.junit.Assert.assertThrows(RuntimeException.class, () -> {
             service.remain(-1);
         });
     }
 
-    @org.testng.annotations.Test
+    @org.junit.Test
     public void testBoundaryValuesAmountMin() {
-        org.testng.Assert.assertEquals(service.remain(0), 1000);
+        org.junit.Assert.assertEquals(1000, service.remain(0));
     }
 
-    @org.testng.annotations.Test
+    @org.junit.Test
     public void testBoundaryValuesAmount() {
-        org.testng.Assert.assertEquals(service.remain(1), 999);
+        org.junit.Assert.assertEquals(999, service.remain(1));
     }
 
-    @org.testng.annotations.Test
+    @org.junit.Test
     public void testBoundaryValuesAmountMax() {
-        org.testng.Assert.assertEquals(service.remain(999), 1);
+        org.junit.Assert.assertEquals(1, service.remain(999));
     }
 
-    @org.testng.annotations.Test
+    @org.junit.Test
     public void testBoundaryValuesMax2() {
-        org.testng.Assert.assertEquals(service.remain(1000), 0);
+        org.junit.Assert.assertEquals(0, service.remain(1000));
     }
 
-    @org.testng.annotations.Test
+    @org.junit.Test
     public void testBoundaryValues2() {
-        org.testng.Assert.assertEquals(service.remain(1001), 999);
+        org.junit.Assert.assertEquals(999, service.remain(1001));
     }
 
-    @org.testng.annotations.Test
+    @org.junit.Test
     public void testRemainCriticalPath() {
-        org.testng.Assert.assertEquals(service.remain(400), 600);
+        org.junit.Assert.assertEquals(600, service.remain(400));
     }
 }
